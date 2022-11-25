bff0f53c7c85cd0d4744b6216f10ebb02585e16b
Dihni Hassan <dihnihassan@gmail.com>
2022-11-24 20:25:02 -0500

    ISS SpotterII

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
 
-  console.log('It worked! Returned IP:', ip);
-});
+//   console.log('It worked! Returned IP:', ip);
+
+// });
+
+
+fetchCoordsByIP("184.147.46.231", (error, data) => {
+  console.log(error, data)});
--- iss.js
+++ iss.js
@@ -1,15 +1,14 @@
 const request = require('request');
 
 
-const fetchMyIP = function(callback) {
+const fetchMyIP = function (callback) {
   request('https://api.ipify.org?format=json', (error, response, body) => {
-    if (error) {
-      return callback(error, null);
-    }
+    if (error) return callback(error, null);
+    
     // console.log(callback);
 
     if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IPD: ${body}`;
+      const msg = `Status Code ${response.statusCode} when fetching IP: ${body}`;
       callback(Error(msg), null);
       return;
     }
@@ -19,5 +18,37 @@ const fetchMyIP = function(callback) {
   });
 };
 
+const fetchCoordsByIP = function (ip, callback) {
+  request('http://ipwho.is/' + ip, (error, response, body) => {
+// console.log("This is for response", Object.keys(response));
 
-module.exports = { fetchMyIP };
+// console.log("This is for body", response.toJSON());
+if (error) {
+  callback(error, null);
+  return;
+}
+
+const data = JSON.parse(body);
+// console.log(data);
+if(!data.success) {
+  const msg = `Success status was ${data.success}. Server message says: ${data.message} when fetching for IP ${data.ip}`;
+callback(Error(msg), null);
+return;
+
+}
+// console.log(data.latitude);
+
+const objCoords = {latitude: data.latitude, longitude: data.longitude};
+
+// console.log(objCoords);
+
+callback(null, objCoords);
+
+})
+};
+ 
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
